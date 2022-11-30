25a908fc910325026c20403dd104e14adf8f8e13
Kelviz <mysviurch@gmail.com>
2022-11-30 04:59:08 +0100

    updated

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+
 if number >= 0:
-    last_digit = number % 10
+    l_digit = number % 10
 else:
-    last_digit = ((number) % -10) 
-msg = "Last digit of " + str(number) + " is " + str(last_digit) + " and is"
-if last_digit == 0:
-    print(msg, "0")
-elif last_digit > 5:
-    print(f"{msg} greater than 5")
+    l_digit = number % -10
+
+print("Last digit of {} is {}".format(number, l_digit), end='')
+
+if l_digit > 5:
+    print(" and is greater than 5")
+elif l_digit == 0:
+    print(" and is 0")
 else:
-     print(f"{msg} less than 6 and not 0")
-        
+    print(" and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    if number >= 0:
-        l_digit = number % 10
+def to_uper(character):
+    if ord(character) >= 97 and ord(character) <= 122:
+        return (ord(character) - 32)
     else:
-        l_digit = number % -10
-        l_digit *= -1
+        return ord(character)
 
-    print("{:d}".format(l_digit), end='')
-    return (l_digit)
+
+def uppercase(string):
+    string_new = ""
+    for character in string:
+        string_new += "%c" % to_uper(character)
+    print("{:s}".format(string_new))
