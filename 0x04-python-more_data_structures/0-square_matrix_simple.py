ecacede8bfb13c86808dc24c1a3d9f69e8f6d337
Kelviz <mysviurch@gmail.com>
2022-12-07 21:31:14 +0100

    0x04

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in matrix:
+        x =[]
+        for n in i:
+            sq_n = n*n
+            x.append(sq_n)
+        new_matrix.append(x)
+    return new_matrix
